cd5cb05d7140d2f03d7ba81f65590909cb0a219b
llindqu <llindqu@kth.se>
2024-06-19 09:54:21 +0200

    Add unite plot & diff colors

--- ui.R
+++ ui.R
@@ -31,13 +31,17 @@ ui <- fluidPage(
   fluidRow(
     column(6, plotOutput("plot_output_1", width = "600px", height = "300px")), # plot from dataset 1 (left)
     column(6, plotOutput("plot_output_2", width = "600px", height = "300px")) # plot from dataset 2 (right)
+  ),
+  fluidRow(
+    column(12, plotOutput("unite", width = "1200px", height = "600px"))
   )
 )
 
-cols <- c("1" = "#14a73f", "2" = '#1ac658', "3" = '#29ea9e', "4" = '#3ae2f4', "5" = '#4beedd', "6" = '#2cb195', "7"="#0d744c", "8"="#1e543e")
-
+cols_1 <- c("1" = "#14a73f", "2" = '#1ac658', "3" = '#29ea9e', "4" = '#3ae2f4', "5" = '#4beedd', "6" = '#2cb195', "7" = "#0d744c", "8" = "#1e543e")
+cols_2 <- c("1" = "#f63c83", "2" = "#d9478c", "3" = "#a25395", "4" = "#b34295", "5" = "#844d9b", "6" = "#59609b", "7" = "#5457a0", "8" = "#2a48b7")
+cols_unite <- c("1.1" = "#14a73f", "1.2" = '#1ac658', "1.3" = '#29ea9e', "1.4" = '#3ae2f4', "1.5" = '#4beedd', "1.6" = '#2cb195', "1.7" = "#0d744c", "1.8" = "#1e543e", "2.1" = "#f63c83", "2.2" = "#d9478c", "2.3" = "#a25395", "2.4" = "#b34295", "2.5" = "#844d9b", "2.6" = "#59609b", "2.7" = "#5457a0", "2.8" = "#2a48b7")
 # function to create the standard cultivation plot (all channels in the same plot)
-std_plot <- \(data, ch_id, LED, x_limit, y_limit) {
+std_plot <- \(data, ch_id, LED, x_limit, y_limit, cols) {
   tmp_df <- subset(data, od_led == LED) %>% filter(channel_id %in% ch_id)
   tmp <- ggplot(tmp_df, aes(x = batchtime_h, y = od_corr, color = as.factor(channel_id))) + theme_bw() + geom_point() +
     theme(legend.position = "top", legend.title = element_blank()) + scale_colour_manual(values = cols) + xlim(x_limit) + ylim(y_limit)
@@ -45,10 +49,26 @@ std_plot <- \(data, ch_id, LED, x_limit, y_limit) {
 }
 
 # function to create the split cultivation plot (one plot per channel)
-split_plot <- \(data, ch_id, LED, x_limit, y_limit) {
+split_plot <- \(data, ch_id, LED, x_limit, y_limit, cols) {
   tmp_df <- subset(data, od_led == LED) %>% filter(channel_id %in% ch_id)
   tmp <- ggplot(tmp_df, aes(x = batchtime_h, y = od_corr, color = as.factor(channel_id))) + theme_bw() + geom_point() +
     theme(legend.position = "top", legend.title = element_blank()) + facet_wrap(vars(channel_id), nrow = 4) + scale_colour_manual(values = cols) + xlim(x_limit) + ylim(y_limit)
+  return(tmp)
+}
+
+# function to create the split cultivation plot (one plot per channel)
+unite_plot <- \(data_one, data_two, ch_id_one, ch_id_two, LED_1, LED_2, x_limit, y_limit) {
+  
+  tmp_df_1 <- subset(data_one, od_led == LED_1) %>% filter(channel_id %in% ch_id_one)
+  tmp_df_2 <- subset(data_two, od_led == LED_2) %>% filter(channel_id %in% ch_id_two)
+  
+  tmp_df_1$channel_id <- as.numeric(paste("1.", tmp_df_1$channel_id, sep = ""))
+  tmp_df_2$channel_id <- as.numeric(paste("2.", tmp_df_2$channel_id, sep = ""))
+  
+  tmp_df <- rbind(tmp_df_1, tmp_df_2)
+  
+  tmp <- ggplot(tmp_df, aes(x = batchtime_h, y = od_corr, color = as.factor(channel_id))) + theme_bw() + geom_point() +
+    theme(legend.position = "top", legend.title = element_blank()) + scale_colour_manual(values = cols_unite) + xlim(x_limit) + ylim(y_limit)
   return(tmp)
 }
 
@@ -76,20 +96,53 @@ server <- function(input, output,session){
   # Rendering of the plot from dataset 1 (left)
   output$plot_output_1 <- renderPlot({
     if (input$split_1 == TRUE) { # if the split checkbox is checked, use the split plot function, otherwise use the standard plot function
-      split_plot(data = data_1(), ch_id = input$channels_1, input$LED1, input$x_lim_1, input$y_lim_1) # call the split plot function
+      split_plot(data = data_1(), ch_id = input$channels_1, input$LED1, input$x_lim_1, input$y_lim_1, cols_1) # call the split plot function
     } else {
-      std_plot(data = data_1(), ch_id = input$channels_1, input$LED1, input$x_lim_1, input$y_lim_1) # call the standard plot function
+      std_plot(data = data_1(), ch_id = input$channels_1, input$LED1, input$x_lim_1, input$y_lim_1, cols_1) # call the standard plot function
     }
   })
   
   output$plot_output_2 <- renderPlot({
     if (input$split_2 == TRUE) { # if the split checkbox is checked, use the split plot function, otherwise use the standard plot function
-      split_plot(data = data_2(), ch_id = input$channels_2, input$LED2, input$x_lim_2, input$y_lim_2) # call the split plot function
+      split_plot(data = data_2(), ch_id = input$channels_2, input$LED2, input$x_lim_2, input$y_lim_2, cols_2) # call the split plot function
     } else {
-      std_plot(data = data_2(), ch_id = input$channels_2, input$LED2, input$x_lim_2, input$y_lim_2) # call the standard plot function
+      std_plot(data = data_2(), ch_id = input$channels_2, input$LED2, input$x_lim_2, input$y_lim_2, cols_2) # call the standard plot function
     }
+  })
+  output$unite <- renderPlot({
+    unite_plot(data_one = data_1(), data_two = data_2(), ch_id_one = input$channels_1, 
+               ch_id_two = input$channels_2, input$LED1, input$LED2, input$x_lim_2, input$y_lim_2)
   })
 }
 
 # run shiny app
 shinyApp(ui=ui,server=server)
+
+
+
+df_1 <- data_list[[1]]
+df_2 <- data_list[[2]]
+channel_id_1 <- df_1$channel_id
+channel_id_2 <- df_2$channel_id
+LED <- 720
+x_limit=c(0,120)
+y_limit=c(0,7)
+cols_unite <- c("1.1" = "#14a73f", "1.2" = '#1ac658', "1.3" = '#29ea9e', "1.4" = '#3ae2f4', "1.5" = '#4beedd', "1.6" = '#2cb195', "1.7" = "#0d744c", "1.8" = "#1e543e", "2.1" = "#f63c83", "2.2" = "#d9478c", "2.3" = "#a25395", "2.4" = "#b34295", "2.5" = "#844d9b", "2.6" = "#59609b", "2.7" = "#5457a0", "2.8" = "#2a48b7")
+
+unite_plot <- \(data_one, data_two, ch_id_one, ch_id_two, LED, x_limit, y_limit) {
+  
+  tmp_df_1 <- subset(data_one, od_led == LED) %>% filter(channel_id %in% ch_id_one)
+  tmp_df_2 <- subset(data_two, od_led == LED) %>% filter(channel_id %in% ch_id_two)
+  
+  tmp_df_1$channel_id <- as.numeric(paste("1.", tmp_df_1$channel_id, sep = ""))
+  tmp_df_2$channel_id <- as.numeric(paste("2.", tmp_df_2$channel_id, sep = ""))
+  
+  tmp_df <- rbind(tmp_df_1, tmp_df_2)
+  
+  tmp <- ggplot(tmp_df, aes(x = batchtime_h, y = od_corr, color = as.factor(channel_id))) + theme_bw() + geom_point() +
+    theme(legend.position = "top", legend.title = element_blank()) + scale_colour_manual(values = cols_unite) + xlim(x_limit) + ylim(y_limit)
+  return(tmp)
+}
+
+unite_plot(df_1, df_2, channel_id_1, channel_id_2, LED, x_limit, y_limit)
+  
